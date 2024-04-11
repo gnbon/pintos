5274f4456a6bb2eabb0335c25ff6d28b9e5521cb
gnbon <kiddo.pwn@gmail.com>
2024-04-11 14:50:53 +0000

    Implement requirement#1 - parse arguments - create robot threads

--- projects/automated_warehouse/automated_warehouse.c
+++ projects/automated_warehouse/automated_warehouse.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "threads/init.h"
 #include "threads/malloc.h"
@@ -32,27 +33,93 @@ void test_thread(void* aux){
         }
 }
 
-// entry point of simulator
+int parse_args(char *task_list, pair *task_pair, int max_count) {
+        char *saveptr;
+        char *token = strtok_r(task_list, ":", &saveptr);
+        int pair_idx = 0;
+
+        task_pair = malloc(sizeof(pair) * max_count);
+        if (task_pair == NULL) {
+                printf("Memory allocation failed\n");
+                return -1;
+        }
+
+        while (token != NULL) {
+                char number = token[0] - '0'; // TODO : check if number is valid
+                char letter = token[1]; // TODO: check if letter is valid
+
+                if (pair_idx >= max_count) {
+                        printf("Too many robots\n");
+                        return -1;
+                }
+
+                if (number < 0 || number > 9) {
+                        printf("Invalid number format: %s\n", token);
+                        return -2; 
+                }
+                
+                if (letter < 'A' || letter > 'Z') {
+                        printf("Invalid letter format: %s\n", token);
+                        return -3;
+                }
+
+                if (token[2]) {
+                        printf("Invalid format: %s\n", token);
+                        return -4;
+                }
+
+                task_pair[pair_idx].cargo = number;
+                task_pair[pair_idx].loading_dock = letter;
+                pair_idx++;
+
+                token = strtok_r(NULL, ":", &saveptr);
+        }
+
+        for (int i = 0; i < pair_idx; i++) {
+                printf("%d%c\n", task_pair[i].cargo, task_pair[i].loading_dock);
+        }
+
+        return 0;
+}
+
+int initialize_robot(int num_robots, pair *task_pair) {
+        struct robot* robots = malloc(sizeof(struct robot) * num_robots);
+        tid_t* threads = malloc(sizeof(tid_t) * num_robots);
+
+        for (int i = 0; i < num_robots; i++) {
+                char rname[3];
+                int idx = i + 1;
+                snprintf(rname, 3, "R%d", i + 1);
+                setRobot(&robots[i], rname, ROW_W, COL_W, 0, 0); // TODO : set robot position and payload
+                threads[i] = thread_create(rname, 0, &test_thread, &idx);
+                if (threads[i] == TID_ERROR) {
+                        printf("Thread creation failed\n");
+                        return -1;
+                }
+        }
+
+        return 0;
+}
+
 void run_automated_warehouse(char **argv)
 {
         init_automated_warehouse(argv); // do not remove this
 
-        printf("implement automated warehouse!\n");
+        int num_robots = atoi(argv[1]);
+        char *task_list = argv[2];
 
-        // test case robots
-        robots = malloc(sizeof(struct robot) * 4);
-        setRobot(&robots[0], "R1", 5, 5, 0, 0);
-        setRobot(&robots[1], "R2", 0, 2, 0, 0);
-        setRobot(&robots[2], "R3", 1, 1, 1, 1);
-        setRobot(&robots[3], "R4", 5, 5, 0, 0);
+        pair* task_pair = NULL;
+        int res = parse_args(task_list, task_pair, num_robots);
+        if (res < 0) {
+                printf("Parsing failed\n");
+                return;
+        }
 
-        // example of create thread
-        tid_t* threads = malloc(sizeof(tid_t) * 4);
-        int idxs[4] = {1, 2, 3, 4};
-        threads[0] = thread_create("CNT", 0, &test_cnt, NULL);
-        threads[1] = thread_create("R1", 0, &test_thread, &idxs[1]);
-        threads[2] = thread_create("R2", 0, &test_thread, &idxs[2]);
-        threads[3] = thread_create("R3", 0, &test_thread, &idxs[3]);
+        res = initialize_robot(num_robots, task_pair);
+        if (res < 0) {
+                printf("Robot initialization failed\n");
+                return;
+        }
 
         // if you want, you can use main thread as a central control node
         
--- projects/automated_warehouse/automated_warehouse.h
+++ projects/automated_warehouse/automated_warehouse.h
@@ -1,9 +1,18 @@
 #ifndef __PROJECTS_AUTOMATED_WAREHOUSE_H__
 #define __PROJECTS_AUTOMATED_WAREHOUSE_H__
 
+typedef struct {
+    unsigned char cargo;
+    char loading_dock;
+} pair;
+
 void test_cnt(void);
 
 void test_thread(void* aux);
+
+int parse_args(char *task_list, pair *task_pair, int max_count);
+
+int initialize_robot(int num_robots, pair *task_pair);
 
 void run_automated_warehouse(char **argv);
 
